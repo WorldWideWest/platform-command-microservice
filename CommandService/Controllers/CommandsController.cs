76362c7f98f1aca9dca4aae81ae9534e28a6ed95
WorldWideWest <dzeno.dzafic@windowslive.com>
2022-06-22 10:54:51 +0200

    Adding controller for creating commands

--- CommandService/Controllers/CommandsController.cs
+++ CommandService/Controllers/CommandsController.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using CommandService.Models.DTOs;
+using CommandService.Models.Entities;
 using CommandService.Models.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,6 +8,7 @@ namespace CommandService.Controllers{
     
     [ApiController]
     [ApiVersion("1.0")]
+    [Produces("application/json")]
     [Route("api/v{varsion:apiVersion}/c/[controller]")]
     public class CommandsController : ControllerBase
     {
@@ -34,6 +36,48 @@ namespace CommandService.Controllers{
             catch (Exception ex)
             {
                 _logger.LogError("GET: Commands/", ex);
+                throw;
+            }
+        }
+
+        [HttpGet("{commandId}", Name = "GetCommandForPlatform")]
+        public ActionResult<CommandResponseDTO> GetCommandForPlatform(Guid platformId, Guid commandId){
+            try
+            {
+                if(!_repository.PlatformExists(platformId))
+                    return NotFound(platformId);
+
+                var result = _repository.GetCommand(platformId, commandId);
+                if(result != null)
+                    return Ok(_mapper.Map<CommandResponseDTO>(result));    
+                return NotFound(commandId);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("GET: Commands/{coomandId}/", ex);
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public ActionResult<CommandResponseDTO> CreateCommandForPlatform(Guid platformId, CommandRequestDTO request){
+            try
+            {
+
+                if(!_repository.PlatformExists(platformId))
+                    return NotFound(platformId);
+                
+                var command = _mapper.Map<Command>(request);
+                _repository.CreateCommand(platformId, command);
+                _repository.SaveChanges();
+
+                var response = _mapper.Map<CommandResponseDTO>(command);
+                return CreatedAtRoute(nameof(GetCommandForPlatform),
+                    new { platformId = platformId, commandId = response.Id }, response);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("GET: Commands/{coomandId}/", ex);
                 throw;
             }
         }
